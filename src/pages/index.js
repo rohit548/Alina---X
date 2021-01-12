5a79610b923ef562df94dcc20b257bc046a0e6b0
rohit548 <77327373+rohit548@users.noreply.github.com>
2021-01-12 15:39:28 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,157 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Icon, Input, List, Hr, Button, Image } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { StackItem, Stack, Section, Override, Menu } from "@quarkly/components";
+import { MdFace } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80) 50% 15%/cover"
+			color="--light"
+			font="--base"
+			hover-font="cvcvcvc"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					Mary
+					<br />
+					Cornell
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				>
+					<Text margin="0px">
+						About me
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						My name is Mary Cornell, born and raised in France.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						I've been working as a professional photographer and videographer for more than 12 years.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						CDS Documentary Essay Prize and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Icon category="md" icon={MdFace} />
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				hello@company.com
+			</Link>
+		</Section>
+		<Icon category="md" icon={MdFace} />
+		<Icon category="md" icon={MdFace} />
+		<Input />
+		<Input />
+		<List>
+			<Text>
+				First item
+			</Text>
+		</List>
+		<Link href="#">
+			Some text
+		</Link>
+		<Box />
+		<Hr />
+		<Button>
+			Button
+		</Button>
+		<Icon category="md" icon={MdFace} />
+		<Image width="64px" height="64px" />
+		<Input />
+		<List>
+			<Text>
+				First item
+			</Text>
+		</List>
+		<Text>
+			Some text
+		</Text>
+		<List>
+			<Text>
+				First item
+			</Text>
+		</List>
+		<Link href="#">
+			Some text
+		</Link>
+		<Link href="#">
+			Some text
+		</Link>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ffd7407a6aceb001f3242df",
+				"5ffd7407a6aceb001f3242e1"
+			]
+		},
+		"5ffd7407a6aceb001f3242df": {
+			"id": "5ffd7407a6aceb001f3242df",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ffd7407a6aceb001f3242e1": {
+			"id": "5ffd7407a6aceb001f3242e1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
